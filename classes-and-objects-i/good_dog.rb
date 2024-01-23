1e57a5d61c64de6c8b64dbbde71a91f5c62fb312
Crystal Lor <crystalalor@gmail.com>
2024-01-23 08:13:01 +0000

    Added more states to track (height and weight) through the attr_accessor method. Also, added a new method called change_info(n, h, w), which allows you to change several states at once. The info method displays all the states of the object, for convenience.

--- classes-and-objects-i/good_dog.rb
+++ classes-and-objects-i/good_dog.rb
@@ -1,17 +1,29 @@
 class GoodDog
-  attr_accessor :name
+  attr_accessor :name, :height, :weight
   
-  def initialize(name)
-    @name = name
+  def initialize(n, h, w)
+    @name = n
+    @height = h
+    @weight = w
   end
   
   def speak
     "#{name} says arf!"
   end
+  
+  def change_info(n, h, w)
+    @name = n
+    @height= h
+    @weight = w
+  end
+  
+  def info
+    "#{name} weighs #{weight} and is #{height} tall."
+  end
 end
 
-sparky = GoodDog.new("Sparky")
-puts sparky.speak
-puts sparky.name
-sparky.name = "Spartacus"
-puts sparky.name
+sparky = GoodDog.new('Sparky', '12 inches', '10 lbs')
+puts sparky.info
+
+sparky.change_info('Spartacus', '24 inches', '45 lbs')
+puts sparky.info
