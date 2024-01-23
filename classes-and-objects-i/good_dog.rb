f23da5ec649b49c1fa933c834b599c2eb4e55897
Crystal Lor <crystalalor@gmail.com>
2024-01-23 08:20:53 +0000

    self.name= lets Ruby know that the setter method is being called, and to not create a local variable instead.

--- classes-and-objects-i/good_dog.rb
+++ classes-and-objects-i/good_dog.rb
@@ -12,9 +12,9 @@ class GoodDog
   end
   
   def change_info(n, h, w)
-    @name = n
-    @height= h
-    @weight = w
+    self.name = n
+    self.height= h
+    self.weight = w
   end
   
   def info
