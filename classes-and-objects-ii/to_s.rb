524115387980405e7d6f99ce0ae233e3485cb6a5
Crystal Lor <crystalalor@gmail.com>
2024-02-06 02:25:06 +0000

    Overriding the #to_s method only works for objects of the type where the customized #to_s method is defined.

--- classes-and-objects-ii/to_s.rb
+++ classes-and-objects-ii/to_s.rb
@@ -1,9 +1,14 @@
-class Foo
+class Bar
+  attr_reader :xyz
+  def initialize
+    @xyz = { a: 1, b:2 }
+  end
+  
   def to_s
-    "42"
+    'I am a Bar object!'
   end
 end
 
-foo = Foo.new
-puts foo
-puts "foo is #{foo}"
+bar = Bar.new
+puts bar
+puts bar.xyz
