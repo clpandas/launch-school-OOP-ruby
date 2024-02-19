8232bc42bbe54180592e6f7dff08c7bc464345f1
Crystal Lor <crystalalor@gmail.com>
2024-02-19 00:31:02 +0000

    It is important to be careful when overriding the Object class's methods. Code may break in other parts of the code that try to access the original intent of an Object class's methods, like send.

---
+++ inheritance/send.rb
@@ -0,0 +1,21 @@
+class Parent 
+  def say_hi
+    p "Hi from Parent."
+  end
+end
+
+class Child < Parent
+  def say_hi
+    p "Hi from Child."
+  end
+  
+  def send
+    p "send from Child..."
+  end
+end 
+
+son = Child.new
+son.send :say_hi
+
+lad = Child.new
+lad.send :say_hi
